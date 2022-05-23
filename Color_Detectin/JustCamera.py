19a4b319b71ac72805dc78e9e0963079d3db694b
AmeenAlShaibani <shaibaniam@gmail.com>
2022-05-22 21:41:17 -0600

    Added script to test camera angle

--- Color_Detectin/JustCamera.py
+++ Color_Detectin/JustCamera.py
@@ -2,16 +2,18 @@ import numpy as np
 import cv2
 from CountsPerSec import CountsPerSec 
 
+
 ##To read only an image for cv2 
-def showCamera():
-    while True:
-        webcam = cv2.VideoCapture(0)
-        _, imageFrame = webcam.read()
-        cv2.imshow("live-feed",imageFrame)
+webcam = cv2.VideoCapture(0)
 
-        if cv2.waitKey(10) & 0xFF == ord('q'):
-                webcam.release()
-                cv2.destroyAllWindows()
-                break
+while True:
+    _, imageFrame = webcam.read()
+    imageFrame = cv2.flip(imageFrame,0)
+    cv2.imshow("live-feed",imageFrame)
 
-showCamera()
+    if cv2.waitKey(10) & 0xFF == ord('q'):
+            webcam.release()
+            cv2.destroyAllWindows()
+            break
+
+#showCamera()
