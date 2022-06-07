ead301532ecd9a2e527d628eb2429b3b7048af50
AmeenAlShaibani <shaibaniam@gmail.com>
2022-06-07 16:17:10 -0600

    Competition Day

--- MotorControl_v2/MotorControl_v6.py
+++ MotorControl_v2/MotorControl_v6.py
@@ -150,7 +150,7 @@ def get_USDATA():
             #55 90 90 55
 
             #THIS EXECUTES AT TIMES WHEN IT SHOULDNT BE DOING SO
-            if((ER < 120 or FR < 120 or FL < 120 or EL < 120) and (mode == "Confirmation") and Avoiding):
+            if((ER < 140 or FR < 140 or FL < 140 or EL < 140) and (mode == "Confirmation") and Avoiding):
                 blocked = True
                 #Turn towards left or right based on which reads a further distance
                 if((SR > SL) and not MovingLeft):
@@ -160,7 +160,7 @@ def get_USDATA():
                     MovingLeft = True
                     RobotMotion.left(200)
 
-            elif((ER > 120 and FR > 120 and FL > 120 and EL > 120) and (mode == "Confirmation")  and Avoiding):
+            elif((ER > 140 and FR > 140 and FL > 140 and EL > 140) and (mode == "Confirmation")  and Avoiding):
                 MovingRight = False
                 MovingLeft = False
                 blocked = False
@@ -699,32 +699,6 @@ def main():
                         time.sleep(4) #TODO: Tune this number
                         RobotMotion.stop()
                         time.sleep(1.5) #TODO Tune htis number
-                
-                
-                #REMOVE THE CAPTURED CODE IF YOU GIVE UP
-                print("Flag capture comfirmation")
-                #go to the Flag zone
-                while(not captured and mode == "Confirmation"):
-                    x1, y1 = get_Position()
-                    if(WithinTolerance(0.5, x1,y1, 18.4, 0.3) and mode == "Confirmation"):
-                        RobotMotion.stop()
-                            captured = True
-                            US_interrupt = True
-                            #capture flag and set next state
-                            closeGrabbingClaw()
-                            time.sleep(1)
-                            liftClaw()
-                            time.sleep(1)
-                            FindingFlag = False
-                            GettingFlag = True
-                    elif(mode == "Confirmation"):
-                        FindHeading(18.4,0.3)
-                        RobotMotion.forward(200)
-                        time.sleep(3) #TODO: Tune this number
-                        RobotMotion.stop()
-                        time.sleep(2)
-                        #TODO Tune htis number
-                
                 
                 #Stop the threads so that the processor can identify the flag
                 stopThreads()
